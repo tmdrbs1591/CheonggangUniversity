72b403da8623042a21698519123466b47c9b4083
tmdrbs1591 <sdh230211@sdh.hs.kr>
2025-06-21 16:25:52 +0900

    추가 : 카메라 무브

---
+++ Assets/00.Main/00.Script/CameraAreaScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine.Utility;
+using Cinemachine;
+
+public class CameraAreaScript : MonoBehaviour
+{
+    [SerializeField]CinemachineConfiner2D confiner;
+  
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            confiner.m_BoundingShape2D = gameObject.GetComponent<PolygonCollider2D>();
+
+
+    }
+}
